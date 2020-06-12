0b662ddc4b67ea3f3e1fb36891f506552519a0fe
rafael-ascari <rafael.ascari@gmail.com>
2020-06-12 11:45:55 -0300

    final da sessao 16

--- 15 RestWithSpringBootUdemy Scaffold Swagger/src/main/java/br/com/erudio/config/WebConfig.java
+++ 15 RestWithSpringBootUdemy Scaffold Swagger/src/main/java/br/com/erudio/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import br.com.erudio.serialization.converter.YamlJackson2HttpMessageConverter;
@@ -17,6 +18,12 @@ public class WebConfig implements WebMvcConfigurer{
 	
 	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
 		converters.add(new YamlJackson2HttpMessageConverter());
+	}
+	
+	public void addCorsMappings(CorsRegistry registry) {
+		// por padrao suporta delete/get/post/put, por isso precisa especificar todos
+		registry.addMapping("/**")
+			.allowedMethods("DELETE", "GET", "POST", "PUT", "PATCH", "OPTIONS", "HEAD", "CONNECT");
 	}
 	
 	@Override
--- 15 RestWithSpringBootUdemy Scaffold Swagger/src/main/java/br/com/erudio/controller/PersonController.java
+++ 15 RestWithSpringBootUdemy Scaffold Swagger/src/main/java/br/com/erudio/controller/PersonController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,8 @@ import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.services.PersonServices;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
+//@CrossOrigin(origins = {"http://localhost:8080", "http://www.abc.com:8080"})
 
 @Api(tags = {"PersonEndpoint"} )
 @RestController
